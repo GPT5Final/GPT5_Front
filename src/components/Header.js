253f4811041bb5b518f90714051ba254454419f6
alstjd974 <alstjd974@nate.com>
2023-04-18 16:25:29 +0900

    LMS_v1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg" style={{ padding: '0.5rem 1rem' }}>
+    <Navbar bg="light" expand="lg" style={{ padding: "0.5rem 1rem" }}>
       <Container>
         <Navbar.Brand>
           <Link to="/">
             <img
               src="./gpt_logo.png"
               style={{
-                width: '6vw',
-                marginRight: '1rem',
+                width: "6vw",
+                marginRight: "1rem",
               }}
               alt="GPT Logo"
             />
@@ -24,24 +24,54 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
-            <Nav.Link as={Link} to="/gyms" className="nav-link" style={{ marginLeft: '5vw' }}>
+            <Nav.Link
+              as={Link}
+              to="/gyms"
+              className="nav-link"
+              style={{ marginLeft: "5vw" }}
+            >
               GYMS
             </Nav.Link>
-            <Nav.Link as={Link} to="/charge" className="nav-link" style={{ marginLeft: '5vw' }}>
-              CHARGE
+            <Nav.Link
+              as={Link}
+              to="/information"
+              className="nav-link"
+              style={{ marginLeft: "5vw" }}
+            >
+              INFORMATION
             </Nav.Link>
-            <Nav.Link as={Link} to="/makeGroup" className="nav-link" style={{ marginLeft: '5vw' }}>
+            <Nav.Link
+              as={Link}
+              to="/makeGroup"
+              className="nav-link"
+              style={{ marginLeft: "5vw" }}
+            >
               MAKEGROUP
             </Nav.Link>
-            <Nav.Link as={Link} to="/stretching" className="nav-link" style={{ marginLeft: '5vw' }}>
+            <Nav.Link
+              as={Link}
+              to="/stretching"
+              className="nav-link"
+              style={{ marginLeft: "5vw" }}
+            >
               STRETCHING
             </Nav.Link>
           </Nav>
-          <Nav className="ms-auto" style={{ marginTop: '-6rem' }}>
-            <Nav.Link as={Link} to="/mypage" className="nav-link" style={{ fontSize: '0.9rem' }}>
+          <Nav className="ms-auto" style={{ marginTop: "-6rem" }}>
+            <Nav.Link
+              as={Link}
+              to="/mypage"
+              className="nav-link"
+              style={{ fontSize: "0.9rem" }}
+            >
               MYPAGE
             </Nav.Link>
-            <Nav.Link as={Link} to="/login" className="nav-link" style={{ fontSize: '0.9rem' }}>
+            <Nav.Link
+              as={Link}
+              to="/login"
+              className="nav-link"
+              style={{ fontSize: "0.9rem" }}
+            >
               LOGIN
             </Nav.Link>
           </Nav>
--- src/routeList.js
+++ src/routeList.js
@@ -1,45 +1,48 @@
-import {
-    createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
-import App from './App';
+import App from "./App";
 import Charge from "./pages/Charge";
 import Gyms from "./pages/Gyms";
 import MakeGroup from "./pages/MakeGroup";
 import Stretching from "./pages/Stretching";
-import Main from './pages/Main';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Information from "./pages/Information";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Main />,
-    },
-    {
-        path: "/login",
-        element: <Login />,
-    },
-    {
-        path: "/register",
-        element: <Register />,
-    },
-    {
-        path: "/gyms",
-        element: <Gyms />
-    },
-    {
-        path: "/charge",
-        element: <Charge />
-    },
-    {
-        path: "/makeGroup",
-        element: <MakeGroup />
-    },
-    {
-        path: "/stretching",
-        element: <Stretching />
-    }
+  {
+    path: "/",
+    element: <Main />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/gyms",
+    element: <Gyms />,
+  },
+  {
+    path: "/information",
+    element: <Information />,
+  },
+  {
+    path: "/charge",
+    element: <Charge />,
+  },
+  {
+    path: "/makeGroup",
+    element: <MakeGroup />,
+  },
+  {
+    path: "/stretching",
+    element: <Stretching />,
+  },
 ]);
 
 export default router;
