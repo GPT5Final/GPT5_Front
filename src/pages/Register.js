b6ac55dc7588d69c7e4ad78c7bda684cbc19d7dd
InchaeNa <dlsco1230@gmail.com>
2023-04-18 16:33:48 +0900

    "PULL"

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import axios from "axios";
+import { Link, useNavigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -8,7 +9,19 @@ import Button from 'react-bootstrap/Button';
 import Header from '../components/Header';
 import { Footer } from '../components/Footer';;
 
+
 function Register() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    nickname: '',
+    name: '',
+    gender: '',
+    birthdate: '',
+    phone: '',
+  });
+
   return (
     <>
       <Header />
